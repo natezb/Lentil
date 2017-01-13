72a6cb117cfede240a71f3af920039dc41047694
Nate Bogdanowicz <natezb@gmail.com>
2017-01-12 18:52:29 -0800

    Convert simple_beam.py example

--- examples/simple_beam.py
+++ examples/simple_beam.py
@@ -1,15 +1,17 @@
-from lentil import plotting as plt, Space, BeamParam
+import matplotlib.pyplot as plt
+from lentil import BeamParam, Lens
 from lentil.plotting import plot_profile
+
 # Indices of refraction
 n0, nc = 1, 2.18
 
 # Create cavity elements
-cavity_elems = [Space('10cm')]
+cavity_elems = [Lens(z='4cm', f='20mm')]
 
 # Find tangential and sagittal cavity modes
 q = BeamParam(wavlen='852nm', z0='2cm', zR='1cm')
 
 # Beam profile inside the cavity
-plot_profile(q, q, cavity_elems, cyclical=True)
+plot_profile(q, q, cavity_elems, z_start='0cm', z_end='10cm', cyclical=True)
 plt.legend()
 plt.show()
